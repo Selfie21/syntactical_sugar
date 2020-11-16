4b7e43c16a093aed3be888c7d207d629781c23f2
Selfie <pierre.brosemer@gmail.com>
2020-11-16 10:25:50 +0100

    added hello world file

---
+++ hello_world.hs
@@ -0,0 +1,19 @@
+module Main
+  where
+
+main = putStrLn "Hello World!"
+
+
+-- simple (simple 2 3 4) 5 6
+-- (simple 2 3 4) * (5+6)
+-- (2*(3+4)) * (5+6)
+-- (2*7) * (5+6)
+-- 14*(5+6)
+-- 14*11
+-- 151
+
+
+--simple (a-b) a b
+--(a-b) * (a+b)
+--aa-ab+ba-bb
+--a^2-b^2
