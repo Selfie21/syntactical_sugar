25ac8227fe9d869b31dc25ad2351bc0ed1f37542
Selfie21 <pierre.brosemer@gmail.com>
2020-11-30 11:46:42 +0100

    finished third exercises

--- ropes.hs
+++ ropes.hs
@@ -1,10 +1,33 @@
 module Ropes where
 
-    data Rope a = Leaf [a] | Inner (Rope a) Int (Rope a)
+  data Rope a = Leaf [a] | Inner (Rope a) Int (Rope a) deriving Show
 
-    helloRope = Inner (Leaf "Hello") 5 (Inner (Leaf ", w") 3 (Leaf "orld!"))
-    numberRope = Inner (Leaf [1,2,3,4]) 4 (Inner (Leaf [1,2]) 2 (Leaf [1,2,3,4,5]))
+  ropeLength :: Rope a -> Int
+  ropeLength (Leaf a) = length a
+  ropeLength (Inner l weight r) = weight + (ropeLength r)
 
-    ropeLength :: Rope a -> Int
-    ropeLength (Leaf a) = length a
-    ropeLength (Inner left weight right) = weight + (ropeLength right)
+  ropeConcat :: Rope a -> Rope a -> Rope a
+  ropeConcat a b = Inner a (ropeLength a) b
+
+
+  ropeSplitAt :: Int -> Rope a -> (Rope a, Rope a)
+  ropeSplitAt index (Leaf a)
+    | otherwise = (Leaf (take index a), Leaf (drop index a))
+
+  ropeSplitAt index (Inner l weight r)
+    | index < weight = let(edgeleft, middleleft) = ropeSplitAt index l
+      in (edgeleft, ropeConcat middleleft r)
+    | index > weight = let(middleright, edgeright) = ropeSplitAt (index-weight) r
+      in (ropeConcat l middleright, edgeright)
+
+
+  ropeInsert :: Int -> Rope a -> Rope a -> Rope a
+  ropeInsert index a b = let (left, right) = ropeSplitAt index b
+    in ropeConcat (ropeConcat left a) right
+
+  
+  ropeDelete :: Int -> Int -> Rope a -> Rope a
+  ropeDelete i j a = let 
+    (ileft, iright) = ropeSplitAt i a
+    (jleft, jright) = ropeSplitAt j a
+    in ropeConcat ileft jright
